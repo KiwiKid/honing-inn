b06d12ee3e434cf00ab77b455a8cbe33006e469d
KiwiKid <5018297+KiwiKid@users.noreply.github.com>
2024-09-08 10:40:58 +1200

    add docker deploy

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "A flake for building, running, and deploying a Go program with devShells";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
